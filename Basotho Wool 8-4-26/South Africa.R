41f98f834b8d3efcc8e459ce9839d3cdbbc88855
Austin Kehr <Kehr0045@d.umn.edu>
2026-08-04 13:28:15 -0500

    Added USD

--- Basotho Wool 8-4-26/South Africa.R
+++ Basotho Wool 8-4-26/South Africa.R
@@ -67,3 +67,31 @@ basotho_wool %>%
   labs(x="Date", y="Basotho Wool Exports (Tonnes)", caption = 
          "Basotho Wool exports from Lesotho to South Africa by month.
           Blue rectangles indicate the southern hemisphere's winter months")
+
+basotho_wool %>% 
+  filter(reporter_code==710) %>% 
+  mutate(date = ymd(ref_period_id)) %>% 
+  ggplot(aes(x=date, y=primary_value))+
+  geom_point()+
+  geom_line()+
+  geom_rect(
+    data = winter,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    inherit.aes = FALSE,
+    fill = "lightblue2",
+    alpha = 0.4
+  )+
+  scale_x_date(
+    limits = c(ymd("2010-01-01"), NA),
+    breaks = seq(
+      from = ymd("2010-01-01"),
+      to = ymd("2025-01-01"),
+      by = "6 months"
+    )
+  )+
+  scale_y_continuous(labels = scales::dollar)+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust=1) )+
+  labs(x="Date", y="Value of Besotho Exports (USD)", caption = 
+         "Value of Basotho Wool Exports from Lesotho to South Africa in USD.")
+
