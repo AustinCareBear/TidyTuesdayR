3f9856e0a5fdef310bd4e226eaeb89e823f3fb8d
Austin Kehr <Kehr0045@d.umn.edu>
2026-08-06 13:27:28 -0500

    Added Continent Breakout, Date Improvement

--- Basotho Wool 8-4-26/United Kingdom.R
+++ Basotho Wool 8-4-26/United Kingdom.R
@@ -5,6 +5,7 @@ library(skimr)
 #Get API Key Here: https://comtradedeveloper.un.org/profile
 #set_primary_comtrade_key("primary key")
 
+#comtrade API data import ----
 #Grab up to 12 months at a time so have to split
 #Same code used in tidytuesdayR for Lesotho just changed to GBR
 united_kingdom_wool_old <- map_df(2010:2020, function(yr) {
@@ -38,10 +39,12 @@ uk_wool<-bind_rows(united_kingdom_wool_old,united_kingdom_wool_new) %>%
   mutate(date = ymd(ref_period_id))
 
 #Alternative method for data:
-#uk_wool<-read_csv("Basotho Wool 8-4-26/UK Wool Data.csv)
+#uk_wool<-read_csv("Basotho Wool 8-4-26/UK Wool Data.csv")
 
-#Overview of data
+#Overview of data ----
 skim_without_charts((uk_wool))
+
+#Monthly visualization ----
 #Data frame for winter months in northern hemisphere used to make blue boxes
 winter <- tibble(
   xmin = ymd(paste0(2009:2024, "-12-01")),
@@ -53,7 +56,7 @@ winter <- tibble(
 #Similar graph as Lesotho and South Africa just with all GBR exports
 uk_wool %>% 
   #20130201 in South Africa was either miss reported or miss entered as it was an extreme outlier
-  filter(reporter_code!=710 & ref_period_id!=20130201) %>% 
+  filter(!(reporter_code==710 & ref_period_id==20130201)) %>% 
   group_by(date) %>% 
   summarise(qty=sum(qty)) %>% 
   ggplot(aes(x=date, y=qty/1000))+
@@ -72,10 +75,40 @@ uk_wool %>%
       from = ymd("2010-01-01"),
       to = ymd("2025-01-01"),
       by = "6 months"
-    )
+    ),
+    date_labels = "%b %Y"
   )+
   theme_classic()+
   theme(axis.text.x = element_text(angle = 45, hjust=1))+
   labs(x="Date",y="Tonnes of Wool", caption = 
          "Wool exported from the Great Britain by month in tonnes.
           The blue boxes indicate winter periods in the northern hemisphere.")
+
+#Broken Out by Continent ----
+to_be_joined<-read_csv("Basotho Wool 8-4-26/Country And Region.csv") 
+
+# RUN ONCE
+uk_wool<-uk_wool  %>% 
+  left_join(to_be_joined, by =c("reporter_iso"="alpha-3") )
+
+rm(to_be_joined)
+
+uk_wool %>% 
+  filter(!(reporter_code == 710 & ref_period_id == 20130201)) %>% 
+  group_by(date, region) %>% 
+  summarise(qty = sum(qty, na.rm = TRUE), .groups = "drop") %>% 
+  ggplot(aes(x = date, y = qty / 1000, color = region)) +
+  geom_line() +
+  geom_point(shape = 19) +
+  scale_x_date(
+    limits = c(ymd("2010-01-01"), NA),
+    breaks = seq(
+      from = ymd("2010-01-01"),
+      to = ymd("2025-01-01"),
+      by = "6 months"
+    ),
+    date_labels = "%b %Y"
+  )+
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Date", y = "Tonnes of Wool",color = "Continent", caption = "Great Britain wool exports broekn out by continent")
