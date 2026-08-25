fcaedbaa968617a5332bac4368d8f4e9dadbf5cc
Austin Kehr <Kehr0045@d.umn.edu>
2026-08-25 10:35:50 -0500

    Basic Graphs

---
+++ Country Music Lyrics 8-25-26/Music.R
@@ -0,0 +1,59 @@
+library(tidytuesdayR)
+library(tidyverse)
+library(tidytext)
+library(skimr)
+
+tuesdata <- tidytuesdayR::tt_load(2026, week = 34)
+
+country_lyrics <- tuesdata$country_lyrics
+top_all_writers <- tuesdata$top_all_writers
+top_primary_writers <- tuesdata$top_primary_writers
+top_producers <- tuesdata$top_producers
+rm(tuesdata)
+
+title_frequencies<-country_lyrics %>% unnest_tokens(split_title,song) %>% 
+  count(split_title,sort=TRUE) %>% 
+  left_join(parts_of_speech,by = c("split_title"= "word"),multiple="first")
+  
+
+lyric_frequencies<-country_lyrics %>% unnest_tokens(split_lyrics,lyrics) %>% 
+  count(split_lyrics,sort=TRUE) %>% 
+  left_join(parts_of_speech, by = c("split_lyrics"="word"),multiple = "first")
+
+title_frequencies %>% 
+  filter(n>10) %>% 
+  ggplot(aes(x=reorder(split_title,n),y=n))+
+  geom_col(aes(fill=pos))+
+  coord_flip()+
+  labs(x="Word",y="Occurences",title="Most Common Words in Country Song Titles",
+       fill="Part of Speech"
+       )+
+  theme_classic()
+
+lyric_frequencies %>% filter(n>750) %>% 
+  ggplot(aes(x=reorder(split_lyrics,n),y=n,fill=pos))+
+  geom_col()+
+  coord_flip()+
+  labs(x="Word",y="Occurences",fill="Part of Speech",
+       title="Most Common Words in Country Song Lyrics")+
+  theme_classic()
+
+title_frequencies %>% 
+  drop_na() %>% 
+  group_by(pos) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=reorder(pos,-count),y=count))+
+  geom_col()+
+  theme_classic()+
+  labs(x="Part of Speech",y="Occurences",title="Part of Speech Occurences in Country Song Titles")+
+  theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))
+
+lyric_frequencies %>% 
+  drop_na() %>%
+  group_by(pos) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=reorder(pos,-count),y=count))+
+  geom_col()+
+  theme_classic()+
+  labs(x="Part of Speech",y="Occurences",title="Part of Speech Occurences in Country Song Lyrics")+
+  theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))
